a56b194fff8a6af9ea9fc1543fcbdceb591578f0
brico1994 <b.rico.barrilero@gmail.com>
2023-02-24 12:19:10 +0100

    .

---
+++ L05.js
@@ -0,0 +1,21 @@
+//en package.json se hace el estart y se apunta al fichero que se uiere ejecutar
+
+var express = require("express");
+var cool = require("cool-ascii-faces");
+
+var app = express();
+var port = 1234;
+
+app.get("/faces", (req,res) => {
+    res.send(cool());
+    console.log(cool());
+})
+
+app.listen(port,() => {
+    console.log(`server ready in ${port}`);
+})
+
+
+
+//hacer npm install para instalar todos los modulos que no estan instalados en local 
+//¡¡¡¡HACERLO SIEMPRE QUE HACEMOS PULL PARA ABRIR EL REPOSITORIO!!!! 
